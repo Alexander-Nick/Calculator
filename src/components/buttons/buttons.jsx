5b500234b108cdb120663f46ba79b04ccefc7d98
Alexander-Nick <opercwal852@gmail.com>
2022-07-18 09:54:02 +0600

    were added memory buttons, + - / * buttons root inverse and clean button.

--- src/components/buttons/buttons.jsx
+++ src/components/buttons/buttons.jsx
@@ -5,69 +5,102 @@ const Buttons = props => {
     input0: () => {
       if (+props.displayValue > 99999999999){
         document.querySelector('.App__error').style.opacity = '1';
-      } else props.state.digitInput('0');
+      } else {
+        props.state.digitInput('0');
+        document.querySelector('.App__error').style.opacity = '0';
+      }
     },
     input1 : () => {
       if (+props.displayValue > 99999999999){
         document.querySelector('.App__error').style.opacity = '1';
-      } else props.state.digitInput('1');
+      } else {
+        props.state.digitInput('1');
+        document.querySelector('.App__error').style.opacity = '0';
+      }
     },
     input2 : () => {
       if (+props.displayValue > 99999999999){
         document.querySelector('.App__error').style.opacity = '1';
-      } else props.state.digitInput('2');
+      } else {
+        props.state.digitInput('2');
+        document.querySelector('.App__error').style.opacity = '0';
+      }
     },
     input3 : () => {
       if (+props.displayValue > 99999999999){
         document.querySelector('.App__error').style.opacity = '1';
-      } else props.state.digitInput('3');
+      } else {
+        props.state.digitInput('3');
+        document.querySelector('.App__error').style.opacity = '0';
+      }
     },
     input4 : () => {
       if (+props.displayValue > 99999999999){
         document.querySelector('.App__error').style.opacity = '1';
-      } else props.state.digitInput('4');
+      } else {
+        props.state.digitInput('4');
+        document.querySelector('.App__error').style.opacity = '0';
+      }
     },
     input5 : () => {
       if (+props.displayValue > 99999999999){
         document.querySelector('.App__error').style.opacity = '1';
-      } else props.state.digitInput('5');
+      } else {
+        props.state.digitInput('5');
+        document.querySelector('.App__error').style.opacity = '0';
+      }
     },
     input6 : () => {
       if (+props.displayValue > 99999999999){
         document.querySelector('.App__error').style.opacity = '1';
-      } else props.state.digitInput('6');
+      } else {
+        props.state.digitInput('6');
+        document.querySelector('.App__error').style.opacity = '0';
+      }
     },
     input7 : () => {
       if (+props.displayValue > 99999999999){
         document.querySelector('.App__error').style.opacity = '1';
-      } else props.state.digitInput('7');
+      } else {
+        props.state.digitInput('7');
+        document.querySelector('.App__error').style.opacity = '0';
+      }
     },
     input8 : () => {
     if (+props.displayValue > 99999999999){
       document.querySelector('.App__error').style.opacity = '1';
-    } else props.state.digitInput('8');
+    } else {
+      props.state.digitInput('8');
+      document.querySelector('.App__error').style.opacity = '0';
+    }
     },
     input9 : () => {
       if (+props.displayValue > 99999999999){
         document.querySelector('.App__error').style.opacity = '1';
-      } else props.state.digitInput('9');
+      } else {
+        props.state.digitInput('9');
+        document.querySelector('.App__error').style.opacity = '0';
+      }
     },
     inputPoint: () => {
       if (+props.displayValue > 99999999999){
         document.querySelector('.App__error').style.opacity = '1';
-      } else props.state.digitInput('.');
+      } else {
+        props.state.digitInput('.');
+        document.querySelector('.App__error').style.opacity = '0';
+      }
     }
   }
   
   return (
     <div className="App__buttons">
-      <button  className="App__button memory">
+      <button onClick={props.state.memoryClean}  className="App__button memory">
         MC
       </button>
       <button onClick={props.state.memoryRead} className="App__button memory">
         MR
       </button>
-      <button  className="App__button memory">
+      <button onClick={props.state.deductFromMemory} className="App__button memory">
         M-
       </button>
       <button onClick={props.state.addInMemory} className="App__button memory">
@@ -88,7 +121,7 @@ const Buttons = props => {
       <button  className="App__button action">
         %
       </button>
-      <button  className="App__button clear">
+      <button onClick={props.state.cleanValue} className="App__button clear">
         C
       </button>
       <button onClick={buttons.input4} className="App__button digit">
@@ -100,11 +133,11 @@ const Buttons = props => {
       <button onClick={buttons.input6} className="App__button digit">
         6
       </button>
-      <button  className="App__button action">
+      <button onClick={props.state.root} className="App__button action">
         √
       </button>
-      <button  className="App__button action">
-        X
+      <button onClick={props.state.multipleOn} className="App__button action">
+        x
       </button>
       <button onClick={buttons.input1} className="App__button digit">
         1
@@ -115,10 +148,10 @@ const Buttons = props => {
       <button onClick={buttons.input3} className="App__button digit">
         3
       </button>
-      <button  className="App__button action">
+      <button onClick={props.state.divideOn} className="App__button action">
         ÷
       </button>
-      <button  className="App__button action">
+      <button onClick={props.state.deductFromValue} className="App__button action">
         -
       </button>
       <button onClick={buttons.input0} className="App__button digit">
@@ -127,13 +160,13 @@ const Buttons = props => {
       <button onClick={buttons.inputPoint} className="App__button digit">
         .
       </button>
-      <button  className="App__button action">
+      <button onClick={props.state.inverseValue} className="App__button action">
         +/-
       </button>
-      <button  className="App__button action">
+      <button onClick={props.state.addValue} className="App__button action">
         +
       </button>
-      <button  className="App__button action">
+      <button onClick={props.state.equal} className="App__button action">
         =
       </button>
     </div>
--- src/state/state.js
+++ src/state/state.js
@@ -12,17 +12,31 @@ const state = {
 
    buttons: {
       memory: '0',
-      addInMemory: () =>{
-         debugger
-         if (state.buttons.memory === '0') state.buttons.memory = '';
-         state.buttons.memory = state.display.displayValue + state.buttons.memory;
-         if (state.buttons.memory !== 0) {
-            document.querySelector('.App__memory').style.opacity = '1'
+      addInMemory: () => {
+         const memory = +state.buttons.memory;
+         const displayValue = +state.display.displayValue;
+         state.buttons.memory = displayValue + memory + '';
+         if (state.buttons.memory !== '0') {
+            document.querySelector('.App__memory').style.opacity = '1';
          } else {
-            document.querySelector('.App__memory').style.opacity = '0'
+            document.querySelector('.App__memory').style.opacity = '0';
          }
       },
-      memoryRead:function () {
+      deductFromMemory: () => {
+         const memory = +state.buttons.memory;
+         const displayValue = +state.display.displayValue;
+         state.buttons.memory = memory - displayValue + '';
+         if (state.buttons.memory !== '0') {
+            document.querySelector('.App__memory').style.opacity = '1';
+         } else {
+            document.querySelector('.App__memory').style.opacity = '0';
+         }
+      },
+      memoryClean: () => {
+         state.buttons.memory = '0';
+         document.querySelector('.App__memory').style.opacity = '0';
+      },
+      memoryRead: function () {
          state.display.displayValue = state.buttons.memory;
          renderDom();
       },
@@ -38,6 +52,92 @@ const state = {
       digitInput: function (digit) {
          if (state.display.displayValue === '0' && digit !== '.') state.display.displayValue = '';
          state.display.displayValue += digit;
+         renderDom();
+      },
+      cleanValue:()=>{
+         document.querySelector('.App__error').style.opacity = '0';
+         if (state.display.displayValue === '0') {
+            state.display.expressionValue = '0';
+         } else {
+            state.display.displayValue = '0';
+         }
+         renderDom();
+      },
+      addValue: () => {
+         if (state.display.expressionValue === '0') {
+            state.display.expressionValue = state.display.displayValue + '+';
+            state.display.displayValue = '0';
+         } else if (state.display.expressionValue !== '0' && state.display.displayValue === '0') {
+            state.display.expressionValue = state.display.expressionValue.slice(0,-1) + '+';
+         } else if (state.display.expressionValue !== '0' && state.display.displayValue !== '0') {
+            //на построение выражений из двух и более действий
+         }
+         renderDom();
+      },
+      deductFromValue: () => {
+         if (state.display.expressionValue === '0') {
+            state.display.expressionValue = state.display.displayValue + '-';
+            state.display.displayValue = '0';
+         } else if (state.display.expressionValue !== '0' && state.display.displayValue === '0') {
+            state.display.expressionValue = state.display.expressionValue.slice(0,-1) + '-';
+         } else if (state.display.expressionValue !== '0' && state.display.displayValue !== '0') {
+            //на построение выражений из двух и более действий
+         }
+         renderDom();
+      },
+      multipleOn: () => {
+         if (state.display.expressionValue === '0') {
+            state.display.expressionValue = state.display.displayValue + '*';
+            state.display.displayValue = '0';
+         } else if (state.display.expressionValue !== '0' && state.display.displayValue === '0') {
+            state.display.expressionValue = state.display.expressionValue.slice(0,-1) + '*';
+         } else if (state.display.expressionValue !== '0' && state.display.displayValue !== '0') {
+            //на построение выражений из двух и более действий
+         }
+         renderDom();
+      },
+      divideOn: () => {
+         if (state.display.expressionValue === '0') {
+            state.display.expressionValue = state.display.displayValue + '/';
+            state.display.displayValue = '0';
+         } else if (state.display.expressionValue !== '0' && state.display.displayValue === '0') {
+            state.display.expressionValue = state.display.expressionValue.slice(0,-1) + '/';
+         } else if (state.display.expressionValue !== '0' && state.display.displayValue !== '0') {
+            //на построение выражений из двух и более действий
+         }
+         renderDom();
+      },
+      inverseValue:()=>{
+         state.display.displayValue = (+state.display.displayValue) * (-1);
+         renderDom();
+      },
+      root:()=>{
+         state.display.displayValue = (+state.display.displayValue) ** (0.5);
+         renderDom();
+      },
+      equal:()=>{
+         const expression = parseFloat(state.display.expressionValue);
+         const value = +state.display.displayValue;
+         const action = state.display.expressionValue.slice(-1);
+         if (action === '+') {
+            state.display.displayValue = value + expression + '';
+            state.display.expressionValue = '0';
+         } else if (action === '-') {
+            state.display.displayValue = expression - value + '';
+            state.display.expressionValue = '0';
+         } else if (action === '/') {
+            if (state.display.displayValue !== '0') {
+               state.display.displayValue = expression / value + '';
+            } else {
+               state.display.displayValue = '0';
+               state.display.expressionValue = '0'
+               document.querySelector('.App__error').style.opacity = '1';
+            }
+            state.display.expressionValue = '0';
+         } else if (action === '*') {
+            state.display.displayValue = expression * value + '';
+            state.display.expressionValue = '0';
+         } 
          renderDom();
       }
    },
