b3d7b52e55d0a43c2c25e7551bb141d2055b76a6
Alexander-Nick <opercwal852@gmail.com>
2022-07-18 08:15:30 +0600

    digits and backspace are working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Buttons from './components/buttons/buttons';
+import Display from './components/display/display';
 
-function App() {
+
+function App(props) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <div className="App__content">
+        <Display state = {props.state.display} />
+        <Buttons state = {props.state.buttons} displayValue = {props.state.display.displayValue} />
+      </div>
+
     </div>
   );
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
+import state from './state/state.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const root = ReactDOM.createRoot(document.getElementById('root'));
+const renderDom = () =>{
+   root.render(<App state ={state} />);
+}
+renderDom();
+state.subscribe(renderDom);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
